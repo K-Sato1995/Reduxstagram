02d7381114208ba1ebfd621ff932641169fbe024
mralwin <owendthiele@gmail.com>
2019-02-24 18:17:37 -0500

    Added App component and provider wrapper

--- src/client/reduxstagram.js
+++ src/client/reduxstagram.js
@@ -2,18 +2,22 @@ import React from "react";
 import { render } from "react-dom";
 import "./styles/style.css";
 import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Provider } from "react-redux";
+import store, { history } from "./store";
 
-import Main from "./components/Main";
+import App from "./components/App";
 import Single from "./components/Single";
 import PhotoGrid from "./components/PhotoGrid";
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid} />
-      <Route path="/view/:postId" component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 );
 
 render(router, document.getElementById("root"));
